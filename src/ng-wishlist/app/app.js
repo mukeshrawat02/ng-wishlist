e96a4d97f4ef1e6b3d69d032ad55b71aaac887a0
Mukesh Rawat <mukeshrawat02@gmail.com>
2016-06-03 22:28:00 +0100

    Added Index file and created basic structure of the app.

---
+++ src/ng-wishlist/app/app.js
@@ -0,0 +1,14 @@
+﻿/**
+ * Created by Mukesh on 06/03/2016.
+ */
+(function () {
+    "use strict";
+
+    var app = angular.module("favListerApp",
+                            [
+                                "common.services",
+                                "ui.router",
+                                "ui.bootstrap"
+                            ]);
+
+}());
