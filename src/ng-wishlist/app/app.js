dff40289a6223473eb3a764dda21e2d4c42486fa
Mukesh Rawat <mukeshrawat02@gmail.com>
2016-06-06 23:35:41 +0100

    added angular ui-router support at index page.

--- src/ng-wishlist/app/app.js
+++ src/ng-wishlist/app/app.js
@@ -11,4 +11,30 @@
                                 "ui.bootstrap"
                             ]);
 
+    app.config(["$stateProvider",
+                "$urlRouterProvider",
+                 function ($stateProvider, $urlRouterProvider) {
+                     $urlRouterProvider.otherwise("/home");
+
+                     $stateProvider
+                         // home
+                         .state("home", {
+                             url: "/home",
+                             templateUrl: "app/home/landingView.html"
+                         })
+                         // login
+                         .state("login", {
+                             url: "/login",
+                             templateUrl: "app/login/loginView.html",
+                             controller: "loginController as self"
+                         })
+                         // registration
+                         .state("register", {
+                             url: "/register",
+                             templateUrl: "app/registration/registrationView.html",
+                             controller: "registrationController as self"
+                         });
+                 }]
+    );
+
 }());
